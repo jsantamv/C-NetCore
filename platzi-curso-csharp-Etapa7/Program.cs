32964dedf3e01342be24efa7d972c22d77ba2708
Juan Carlos Santamaria Villegas <jsantamv@live.com>
2019-11-13 23:22:43 -0600

    Inicio

---
+++ platzi-curso-csharp-Etapa7/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CoreEscuela.Entidades;
+using CoreEscuela.Util;
+using static System.Console;
+
+namespace CoreEscuela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppDomain.CurrentDomain.ProcessExit += AccionDelEvento;
+            AppDomain.CurrentDomain.ProcessExit += (o, s) => Printer.Beep(2000, 1000, 1);
+
+            var engine = new EscuelaEngine();
+            engine.Inicializar();
+            Printer.WriteTitle("BIENVENIDOS A LA ESCUELA");
+            //Printer.Beep(10000, cantidad: 10);
+            //ImpimirCursosEscuela(engine.Escuela);
+            Dictionary<int, string> dicccionario = new Dictionary<int, string>();
+
+            dicccionario.Add(10, "JuanK");
+
+            dicccionario.Add(23, "Lorem Ipsum");
+
+            foreach (var keyValPair in dicccionario)
+            {
+                WriteLine($"Key: {keyValPair.Key} Valor: {keyValPair.Value}");
+            }
+
+            var dictmp = engine.GetDiccionarioObjetos();
+
+            engine.ImprimirDiccionario(dictmp, true);
+
+        }
+
+        private static void AccionDelEvento(object sender, EventArgs e)
+        {
+            Printer.WriteTitle("SALIENDO");
+            Printer.Beep(3000, 1000, 3);
+            Printer.WriteTitle("SALIÓ");
+        }
+
+        private static void ImpimirCursosEscuela(Escuela escuela)
+        {
+
+            Printer.WriteTitle("Cursos de la Escuela");
+
+
+            if (escuela?.Cursos != null)
+            {
+                foreach (var curso in escuela.Cursos)
+                {
+                    WriteLine($"Nombre {curso.Nombre  }, Id  {curso.UniqueId}");
+                }
+            }
+        }
+    }
+}
